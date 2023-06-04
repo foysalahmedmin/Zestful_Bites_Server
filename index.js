c5aa34903976f6bed496d77784f3e5d6698b6380
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-06-05 03:23:41 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
@@ -6,6 +7,42 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization ;
+}
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.h9wx0u0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    const database = client.db("ZestfulDB");
+    const MenuCollection = database.collection("MenuCollection");
+    // const ReviewCollection = database.collection("ReviewMenuCollection");
+    app.get('/menu', async (req, res) => {
+      const result = await MenuCollection.find().toArray()
+      res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
